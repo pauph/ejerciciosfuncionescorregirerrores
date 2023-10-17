4192abf1032513415b0a1e00d3542f081bbb9daa
Ana Paula Percino Huerta <128005733+pauph@users.noreply.github.com>
2023-10-17 12:01:34 -0600

    Add files via upload

---
+++ ejercicio1errores.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int suma(int a, int b);
+
+int suma(int a, int b)
+{
+	int add;
+	add = a + b;
+	return add;
+}
+
+int main()
+{
+	int num1, num2, sum;
+	printf("Introducir dos numeros a sumar \n");
+	scanf("%d %d", &num1, &num2);
+	sum = suma(num1, num2);
+	printf("La suma es: %d", sum);
+	return 0;
+	
+}
---
+++ ejericio2encontrarerrores.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+float perimetroFigura(float numero)
+{
+	float perimetro;
+	perimetro = 4 * numero;
+	return perimetro;
+}
+
+float areaFigura(float numero)
+{
+	float area;
+	area = numero * numero;
+	return area;
+}
+
+int main()
+{
+	int num;
+	printf("Lado de un cuadrado: ");
+	scanf("%d", &num);
+	printf("Perimetro: %f\n", perimetroFigura(num));
+	printf("Area: %f\n", areaFigura(num));
+	return 0;
+}
